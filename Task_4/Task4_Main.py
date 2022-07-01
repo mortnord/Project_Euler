2455dc68c33bc58b2c54b57581712f1616451a20
mortnord <mortnord@gmail.com>
2022-07-01 14:18:41 +0200

    Muligens ferdig

---
+++ Task_4/Task4_Main.py
@@ -0,0 +1,32 @@
+import copy
+
+
+def palindrome_Checker(palindrome):
+    reversed_palindrome = palindrome
+    reversed_palindrome = str(reversed_palindrome)[::-1]
+    if int(reversed_palindrome) == int(palindrome):
+        return True
+    else:
+        return False
+
+
+def largest_palindrome_product():
+    nr_1 = 999
+    nr_2 = 999
+    largest_palindrome = 0
+    while nr_2 > 0:
+        palindrome = nr_1*nr_2
+        if palindrome_Checker(palindrome):
+            largest_palindrome = nr_1*nr_2
+            print(largest_palindrome)
+            nr_1 -= 1
+        else:
+            nr_1 -= 1
+            if nr_1 == 0:
+                nr_1 = 999
+                nr_2 -= 1
+
+
+#A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+#Find the largest palindrome made from the product of two 3-digit numbers.
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import Task_1.Task1_Main
 import Task_2.Task2_Main
 import Task_3.Task3_Main
+import Task_4.Task4_Main
 
 print("Skriv nr på task for å gå til den")
 select_nr = input()
@@ -11,3 +12,5 @@ elif select_nr == "2":
     Task_2.Task2_Main.even_fibonacci_numbers()
 elif select_nr == "3":
     Task_3.Task3_Main.largest_prime_factor()
+elif select_nr == "4":
+    Task_4.Task4_Main.largest_palindrome_product()
